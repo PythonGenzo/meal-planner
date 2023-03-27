37cde43391736913aceb4d402e612d765552c722
PythonGenzo <sanprasanth94@gmail.com>
2023-03-27 16:21:08 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Meal-planner
+
+![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB) &nbsp;![Bootstrap](https://img.shields.io/badge/bootstrap-%23563D7C.svg?style=for-the-badge&logo=bootstrap&logoColor=white) &nbsp;![React Router](https://img.shields.io/badge/React_Router-CA4245?style=for-the-badge&logo=react-router&logoColor=white)
+
+#### Explanation  
+   <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Meal planner app created using React Js. 
+By giving calories, the app will provide you the daily meal plan for your healthier life.</p>
+
+
+#### Screenshots  
+
+**Meal planner page** 
+
+<img src="" title="Meal planner page">
+
---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import MealList from "./MealList";
+
+function App() {
+  const [mealData, setMealData] = useState(null);
+  const [calories, setCalories] = useState(2000);
+
+  function getMealData() {
+    fetch(
+      `https://api.spoonacular.com/mealplanner/generate?apiKey=41f7abb546b94e8f8dd8bf6a653f6676&timeFrame=day&targetCalories=${calories}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setMealData(data);
+      })
+      .catch(() => {
+        console.log("error");
+      });
+  }
+
+  function handleChange(e) {
+    setCalories(e.target.value);
+  }
+
+  return (
+    <div className="App">
+      <button style={{fontsize:"20px"}}>WELCOME TO PRASANTH MEAL PLANNER APP!!</button>
+      <section className="controls">
+        <input
+          type="number"
+          placeholder="Enter Calories (e.g. 2000)"
+          onChange={handleChange}
+        />
+        <button onClick={getMealData}>Get Daily Meal Plan</button>
+      </section>
+      {mealData && <MealList mealData={mealData} />}
+    </div>
+  );
+}
+
+export default App;
